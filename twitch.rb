04675e318b324405d1504f05bee1d0e2951ed4d6
Twan Wolthof <xeago@spotify.com>
2018-05-08 20:46:55 +0000

    Fix stream

--- twitch.rb
+++ twitch.rb
@@ -10,29 +10,27 @@ require 'concurrent'
 require 'fileutils'
 
 
-module Stream
-  def token_uri(channel)
-    URI.parse("http://api.twitch.tv/api/channels/#{channel}/access_token")
+def token_uri(channel)
+  URI.parse("http://api.twitch.tv/api/channels/#{channel}/access_token")
+end
+def token(channel)
+  s = Net::HTTP.start('api.twitch.tv', use_ssl: true) do |http|
+     http.get(token_uri(channel), {'Client-ID' => 'jzkbprff40iqj646a697cyrvl0zt2m6'})
   end
-  def token(channel)
-    s = Net::HTTP.start('api.twitch.tv', use_ssl: true) do |http|
-       http.get(token_uri(channel), {'Client-ID' => 'jzkbprff40iqj646a697cyrvl0zt2m6'})
-    end
-    JSON.parse(s.body)
-  end
+  JSON.parse(s.body)
+end
 
-  def stream_uri(channel)
-    base = "http://usher.justin.tv/api/channel/hls/"
-    channel_streamer = File.basename channel
-    token = token(channel_streamer)
-    uri = URI("#{base}#{channel_streamer}")
-    uri.query = URI.encode_www_form({
-      sig: token["sig"],
-      token: token["token"],
-      allow_source: true,
-    })
-    uri
-  end
+def stream_uri(channel)
+  base = "http://usher.justin.tv/api/channel/hls/"
+  channel_streamer = File.basename channel
+  token = token(channel_streamer)
+  uri = URI("#{base}#{channel_streamer}")
+  uri.query = URI.encode_www_form({
+    sig: token["sig"],
+    token: token["token"],
+    allow_source: true,
+  })
+  uri
 end
 
 def to_json(obj)
@@ -42,7 +40,7 @@ end
 class TwitchRb < Thor
   desc "stream CHANNEL", "output an m3u8 suitable for VLC"
   def stream(channel)
-    uri = Stream.stream_uri(channel)
+    uri = stream_uri(channel)
     m3u8 = Net::HTTP.get(uri)
     m3u8.display
   end
