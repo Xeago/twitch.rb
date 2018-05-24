51041cbcfe2f0298ab757d783a51fd8d93af4164
Twan Wolthof <xeago@spotify.com>
2018-05-24 13:39:43 +0000

    Write vods.json
    This should remove the caddy dependency for browsebirb.
    Mirroring the archive can still be done as long as *any* sort of listing is provided.

--- twitch.rb
+++ twitch.rb
@@ -111,6 +111,20 @@ class TwitchRb < Thor
     end
     pool.shutdown
     pool.wait_for_termination
+    vods = Dir[archive_path + '/*/*/*/video.json'].map do |v|
+      playable = File.exist?(File.dirname(v) + "/index.m3u8") ? true : false
+      {
+        video: v,
+        playable: playable
+      }
+    end.group_by do |vod|
+      user_name, uuid, numbers = vod[:video].match(
+        /#{Regexp.quote(archive_path)}\/(?<user_id>[^ _\/]+)\/(?<uuid>[^ _\/]+)\/(?<numbers>[^ \/]+)\/video.json/).captures;
+      user_name
+    end
+    File.open(archive_path + '/vods.json', 'w+')  do |file|
+      file.write(JSON.dump(vods))
+    end
   end
 end
 
